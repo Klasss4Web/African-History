174b6094cba8edab8fb2b22731d2f28722e7c62a
emmanuel.ochade <emmanuel.ochade@interswitchng.com>
2025-08-16 14:05:53 +0100

    added manifest json file to the pwa

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,17 @@ import "./index.css";
 // import "./styles/globals.css";
 import App from "./App.tsx";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log("Service Worker registered:", registration);
+      })
+      .catch((err) => console.log("SW registration failed:", err));
+  });
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
